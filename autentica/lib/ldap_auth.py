9f2d0316e64f96b9a777a0ed5584d59e2885fb82
aleodoni <aleodoni@gmail.com>
2021-04-29 21:38:24 -0300

    fix: Corrige problema de busca de usuário quando o mesmo existe no BD mas o cpf está nulo

--- autentica/lib/ldap_auth.py
+++ autentica/lib/ldap_auth.py
@@ -7,59 +7,39 @@ import logging
 class AuthBackend:
   def authenticate(self, request, username=None, password=None):
     logger = logging.getLogger(__name__)
-    # try:
-    l = ldap.initialize(settings.LDAP_AUTH_URL, bytes_mode=False)
-    l.protocol_version = ldap.VERSION3
+    try:
+      l = ldap.initialize(settings.LDAP_AUTH_URL, bytes_mode=False)
+      l.protocol_version = ldap.VERSION3
 
-    l.simple_bind_s("uid={},{}".format(username, settings.LDAP_AUTH_SEARCH_BASE), password)
+      l.simple_bind_s("uid={},{}".format(username, settings.LDAP_AUTH_SEARCH_BASE), password)
 
-    l.simple_bind_s("cn={},{}".format(settings.LDAP_BIND_USERNAME, settings.LDAP_AUTH_BIND_BASE),settings.LDAP_BIND_PASSWORD)
+      l.simple_bind_s("cn={},{}".format(settings.LDAP_BIND_USERNAME, settings.LDAP_AUTH_BIND_BASE),settings.LDAP_BIND_PASSWORD)
 
-    results = l.search_s(settings.LDAP_AUTH_SEARCH_BASE, ldap.SCOPE_SUBTREE, "uid={}".format(username))
+      results = l.search_s(settings.LDAP_AUTH_SEARCH_BASE, ldap.SCOPE_SUBTREE, "uid={}".format(username))
 
-    logger.info(results)
+      # logger.info(results)
 
-    # uid = results[0][1]['uid'][0].decode('utf-8')
-    # cpf = results[0][1]['employeeNumber'][0].decode('utf-8')
-    # email = results[0][1]['mail'][0].decode('utf-8')
-    # username = results[0][1]['uid'][0].decode('utf-8')
-    # first_name = results[0][1]['givenName'][0].decode('utf-8')
-    # last_name = results[0][1]['sn'][0].decode('utf-8')
-
-    uid = results[0][1]['uid'][0]
-    cpf = results[0][1]['employeeNumber'][0]
-    email = results[0][1]['mail'][0]
-    # username = results[0][1]['uid'][0]
-    first_name = results[0][1]['givenName'][0]
-    last_name = results[0][1]['sn'][0]
-
-    logger.info(uid)
-    logger.info(cpf)
-    logger.info(email)
-    logger.info(first_name)
-    logger.info(last_name)
-
-    logger.info(uid.decode())
-    logger.info(cpf.decode())
-    logger.info(email.decode())
-    logger.info(first_name.decode())
-    logger.info(last_name.decode())
-
-    # except:
-      # return None
+      uid = results[0][1]['uid'][0].decode()
+      cpf = results[0][1]['employeeNumber'][0].decode()
+      email = results[0][1]['mail'][0].decode()
+      first_name = results[0][1]['givenName'][0].decode()
+      last_name = results[0][1]['sn'][0].decode()
+    except:
+      return None
 
     try:
       user = Usuario.objects.get(username=username)
+      user.cpf = cpf
     except Usuario.DoesNotExist:
       user = Usuario(username=username)  
-      user.uid = uid.decode()
-      user.cpf = cpf.decode()
-      user.email = email.decode()
+      user.uid = uid
+      user.cpf = cpf
+      user.email = email
       user.username = username
-      user.first_name = first_name.decode()
-      user.last_name = last_name.decode()
+      user.first_name = first_name
+      user.last_name = last_name
       user.save()
-    
+
     return user
 
   def get_user(self, user_id):
